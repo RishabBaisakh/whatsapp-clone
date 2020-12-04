82fa123c63e389e8ef4d1b3a54f7e7e35ddc884a
RishabBaisakh <rishab.baisakh@gmail.com>
2020-12-03 21:17:44 -0500

    fixed git issue

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAibnR9Su0zqyTjq-sUVIP46mt3t3LY3KQ",
+  authDomain: "whatsapp-mern-5bd34.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-5bd34.firebaseio.com",
+  projectId: "whatsapp-mern-5bd34",
+  storageBucket: "whatsapp-mern-5bd34.appspot.com",
+  messagingSenderId: "708079210095",
+  appId: "1:708079210095:web:a65d982c27c437c20f079a",
+  measurementId: "G-64RJW8EN4S",
+};
