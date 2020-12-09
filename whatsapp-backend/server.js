3c587405501b587fc8ae2512084fa5ef04eb095e
RishabBaisakh <rishab.baisakh@gmail.com>
2020-12-09 18:45:55 -0500

    added a create api and connected to mongoDB

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,21 +1,43 @@
 // => importing....
 import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
 
 // => app config....
-// creating the application that allows us to api routes....
 const app = express();
-// first part is production friendly....
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 8080;
 
 // => middleware....
+app.use(express.json());
 
 // => DB config....
+// mongodb+srv://admin:<password>@cluster0.5j2ed.mongodb.net/<dbname>?retryWrites=true&w=majority
+const connection_url =
+  "mongodb+srv://admin:rt941olDHz09mpkm@cluster0.5j2ed.mongodb.net/whatsappdb?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // => ?????
+// admin password => rt941olDHz09mpkm
 
 // => api routes....
+app.get("/", (req, res) => res.status(200).send("hello world"));
 
-app.get("/", (req, res) => req.status(200).send("hello world"));
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(`new message created : \n ${data}`);
+    }
+  });
+});
 
 // => listener....
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
